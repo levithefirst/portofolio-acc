39ff1b6e4c2262d5af34143617d0aab5ead19ed5
levithefirst <leviweb3x@gmail.com>
2026-07-18 16:43:22 +0100

    Update PostCSS configuration for Tailwind and Autoprefixer

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [{ protocol: "https", hostname: "unavatar.io" }],
-  },
-};
-export default nextConfig;
+export default { plugins: { tailwindcss: {}, autoprefixer: {} } };
