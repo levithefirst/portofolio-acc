ae94fb3657c6f3b094832b4802faeeb2874e1416
levithefirst <leviweb3x@gmail.com>
2026-07-18 15:59:31 +0100

    Create postcss.config.mjs

---
+++ postcss.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "unavatar.io" }],
+  },
+};
+export default nextConfig;
